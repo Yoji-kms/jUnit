54a48f54df1842383bf27312758c49c884c61cea
Mashnin Egor <y0g@mail.ru>
2020-10-14 00:58:25 +1000

    Initial commit

---
+++ src/main/kotlin/ru.netology/WallService.kt
@@ -0,0 +1,47 @@
+package ru.netology
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
+
+    fun clear() {
+        posts = emptyArray()
+        comments = emptyArray()
+    }
+
+    fun createComment(comment: Comment, postId: Int): Boolean {
+        val post:Post? = findPostById(postId)
+
+        if (post != null) {
+            comments += comment
+            update(post.copy(comments = Comments(comments = comments)))
+        }
+        return true
+    }
+
+    private fun findPostById(postId: Int): Post?{
+        for(post:Post in posts){
+            if (post.id == postId) return post
+        }
+        throw PostNotFoundException()
+    }
+
+    fun add(post: Post): Post {
+        val newPost = post.copy(id = posts.lastIndex + 1)
+        posts += newPost
+        return newPost
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, updatingPost) in posts.withIndex()) {
+            if (index == post.id) {
+                posts[index] = post.copy(
+                    ownerId = updatingPost.ownerId,
+                    date = updatingPost.date
+                )
+                return true
+            }
+        }
+        return false
+    }
+}
